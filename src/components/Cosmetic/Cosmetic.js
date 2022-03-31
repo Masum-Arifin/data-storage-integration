8637c918f5f9f191054594deaed20bcfb14772fa
Masum-Arifin <masumarifinbd@gmail.com>
2022-03-31 14:48:04 +0600

    newt commit

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -3,6 +3,12 @@ import "./Cosmetic.css";
 
 const Cosmetic = (props) => {
   const { name, price, id } = props.cosmetic;
+  const addToCart = (id) =>{
+      console.log('item added',id);
+  }
+
+//    const addToCartWithParm = () => addToCart(id)
+
   return (
     <div className="product">
       <h2>Buy this: {name}</h2>
@@ -10,6 +16,8 @@ const Cosmetic = (props) => {
       <p>
         <small>it has id:{id}</small>
       </p>
+      {/* <button onClick={addToCartWithParm}>Add to Cart</button> */}
+      <button onClick={() => addToCart(id)}>Purchase</button>
     </div>
   );
 };
