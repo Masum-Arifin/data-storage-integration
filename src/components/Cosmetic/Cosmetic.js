7f881487937a1dc04af6a8b0e4cda441bcdf615e
Masum-Arifin <masumarifinbd@gmail.com>
2022-03-31 14:29:10 +0600

    newt commit

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
+import "./Cosmetic.css";
 
 const Cosmetic = (props) => {
-    const {name
-        , price, id} = props.cosmetic;
-    return (
-        <div>
-            <h2>Buy this: {name}</h2>
-            <p>Only for: $ {price}</p>
-            <p><small>it has id:{id}</small></p>
-        </div>
-    );
+  const { name, price, id } = props.cosmetic;
+  return (
+    <div className="product">
+      <h2>Buy this: {name}</h2>
+      <p>Only for: $ {price}</p>
+      <p>
+        <small>it has id:{id}</small>
+      </p>
+    </div>
+  );
 };
 
 export default Cosmetic;
--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,26 +1,26 @@
-// import React from "react";
-// import { useEffect } from "react";
-// import { useState } from "react";
-// import Cosmetic from "../Cosmetic/Cosmetic";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import Cosmetic from "../Cosmetic/Cosmetic";
 
-// const Cosmetics = () => {
-//     const [cosmetics,setCosmetics] = useState([]);
+const Cosmetics = () => {
+    const [cosmetics,setCosmetics] = useState([]);
 
-//     useEffect(() => {
-//        fetch('data.json')
-//        .then(res=> res.json())
-//        .then(data=> setCosmetics(data))  
-//     }, [])
-//   return (
-//     <div>
-//       <h1>Welcome to my cosmetics store</h1>
-//       {
-//           cosmetics.map(cosmetic => <Cosmetic key={cosmetic.id}
-//           cosmetic = {cosmetic}
-//           ></Cosmetic>)
-//       }
-//     </div>
-//   );
-// };
+    useEffect(() => {
+       fetch('data.json')
+       .then(res=> res.json())
+       .then(data=> setCosmetics(data))  
+    }, [])
+  return (
+    <div>
+      <h1>Welcome to my cosmetics store</h1>
+      {
+          cosmetics.map(cosmetic => <Cosmetic key={cosmetic.id}
+          cosmetic = {cosmetic}
+          ></Cosmetic>)
+      }
+    </div>
+  );
+};
 
-// export default Cosmetics;
+export default Cosmetics;
