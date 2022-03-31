2e9ccfa7722e54d98b99554893cc7bb740af7f11
Masum-Arifin <masumarifinbd@gmail.com>
2022-03-31 18:38:11 +0600

    new code

--- src/utilities/calculate.js
+++ src/utilities/calculate.js
@@ -2,4 +2,9 @@ const add = (first , second) => {
     return first + second;
 }
 
+
+
+// const number =  [20, 24, 235, 89]
+// const sumReducer = (previous, current) => previous + current;
+// const total = number.reduce(sumReducer, 0)
 export default add;
